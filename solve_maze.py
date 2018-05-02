f5f1f7bb28d46a7460780455b0dd921806675eab
Elliot Briant <briantmoliveira@gmail.com>
2018-05-02 14:34:59 -0700

    fineshed solving the maze

--- solve_maze.py
+++ solve_maze.py
@@ -6,13 +6,48 @@ import sys
 # Solve maze using Pre-Order DFS algorithm, terminate with solution
 def solve_dfs(m):
     # TODO: Implement solve_dfs
-    pass
+    stack = []
+    current_cell = 0
+    visited_cells = 0
+
+    while current_cell < m.total_cells -1:
+        unvisited_neighbors = m.cell_neighbors(current_cell)
+        if len(neighbors) > 0:
+            neighbors_index = m.random.randint(0, len(neighbors) - 1)
+            new_cell = neighbors[neighbors_index][0]
+            visit = visit_cells(new_cell)
+            stack.append(current_cell)
+            current_cell = new_cell
+            visited_cells += 1
+        else:
+            backtrack_cell = backtrack(current_cell)
+            current_cell = stack.pop()
+        m.refresh_maze_view()
+    m.state = 'idle'
+
 
 
 # Solve maze using BFS algorithm, terminate with solution
 def solve_bfs(m):
     # TODO: Implement solve_bfs
-    pass
+    queue = Queue()
+    current_cell = 0
+    in_direction = 0b0000
+    visited_cells = 0
+    queue.enqueue(current_cell, in_direction)
+
+    while (current_cell != m.total_cells - 1) and not (queue.isEmpty()):
+        current_cell, in_direction = queue.dequeue
+        visit = bfs_visit_cell(current_cell)
+        visited_cells += 1
+        m.refresh_maze_view()
+
+        unvisited_neighbors = m.cell_neighbors(current_cell)
+        for neighbor in neighbors:
+            queue.enqueue(neighbor)
+    m.reconstruct_solution(current_cell)
+
+    m.state = 'idle'
 
 
 def print_solution_array(m):
